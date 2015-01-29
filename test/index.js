b59921c917d1b188cba40bcc87885d9b4f64759d
Dimitrios C. Michalakos <dimitris@jmike.gr>
2015-01-29 23:29:04 +0200

    Refactor naomi.js and conform to airbnb guidelines

--- test/index.js
+++ test/index.js
@@ -1,20 +1,19 @@
-var path = require('path'),
-  Mocha = require('mocha'),
-  mocha;
+var path = require('path');
+var Mocha = require('mocha');
 
 // init mocha
-mocha = new Mocha({
+var mocha = new Mocha({
   reporter: 'spec',
   timeout: 30000 // 30 secs
 });
 
 // load the test files
 mocha.addFile(path.resolve(__dirname, './naomi'));
-mocha.addFile(path.resolve(__dirname, './mysql-database'));
+// mocha.addFile(path.resolve(__dirname, './mysql-database'));
 // mocha.addFile(path.resolve(__dirname, './mysql-querybuilder'));
 // mocha.addFile(path.resolve(__dirname, './mysql-table'));
 // mocha.addFile(path.resolve(__dirname, './mysql-transaction'));
-mocha.addFile(path.resolve(__dirname, './postgres-database'));
+// mocha.addFile(path.resolve(__dirname, './postgres-database'));
 // mocha.addFile(path.resolve(__dirname, './postgres-querybuilder'));
 // mocha.addFile(path.resolve(__dirname, './postgres-table'));
 // mocha.addFile(path.resolve(__dirname, './postgres-transaction'));
--- test/naomi.js
+++ test/naomi.js
@@ -1,18 +1,43 @@
-var chai = require('chai'),
-  naomi = require('../src/naomi'),
-  Database = require('../src/Database'),
-  MySQLDatabase = require('../src/mysql/Database'),
-  PostgresDatabase = require('../src/postgres/Database'),
-  assert = chai.assert;
+var assert = require('chai').assert;
+var naomi = require('../src/naomi');
+var Database = require('../src/Database');
+var MySQLDatabase = require('../src/mysql/Database');
+var PostgresDatabase = require('../src/postgres/Database');
 
 describe('naomi', function () {
 
   describe('#create()', function () {
 
+    it('throws error when engine is invalid', function () {
+      assert.throws(function () { naomi.create(123); }, /invalid engine argument/i);
+      assert.throws(function () { naomi.create(false); }, /invalid engine argument/i);
+      assert.throws(function () { naomi.create(null); }, /invalid engine argument/i);
+      assert.throws(function () { naomi.create({}); }, /invalid engine argument/i);
+      assert.throws(function () { naomi.create(new Date()); }, /invalid engine argument/i);
+    });
+
+    it('throws error when engine is unknown', function () {
+      assert.throws(function () { naomi.create('unknown'); }, /unknown engine/i);
+    });
+
     it('returns new Database when type is mysql', function () {
       var db = naomi.create('mysql');
       assert.instanceOf(db, Database);
       assert.instanceOf(db, MySQLDatabase);
+    });
+
+    it('returns new Database when type is postgres', function () {
+      var db = naomi.create('postgres');
+      assert.instanceOf(db, Database);
+      assert.instanceOf(db, PostgresDatabase);
+    });
+
+    it('throws error when props is invalid', function () {
+      assert.throws(function () { naomi.create('mysql', 123); }, /invalid props argument/i);
+      assert.throws(function () { naomi.create('mysql', false); }, /invalid props argument/i);
+      assert.throws(function () { naomi.create('mysql', null); }, /invalid props argument/i);
+      assert.throws(function () { naomi.create('mysql', 'string'); }, /invalid props argument/i);
+      assert.throws(function () { naomi.create('mysql', new Date()); }, /invalid props argument/i);
     });
 
     it('loads MySQL connection properties from environmental variables', function () {
@@ -24,12 +49,6 @@ describe('naomi', function () {
       assert.strictEqual(db.connectionProperties.database, process.env.MYSQL_DATABASE);
     });
 
-    it('returns new Database when type is postgres', function () {
-      var db = naomi.create('postgres');
-      assert.instanceOf(db, Database);
-      assert.instanceOf(db, PostgresDatabase);
-    });
-
     it('loads Postgres connection properties from environmental variables', function () {
       var db = naomi.create('postgres');
       assert.strictEqual(db.connectionProperties.host, process.env.POSTGRES_HOST);
@@ -37,14 +56,6 @@ describe('naomi', function () {
       assert.strictEqual(db.connectionProperties.user, process.env.POSTGRES_USER);
       assert.strictEqual(db.connectionProperties.password, process.env.POSTGRES_PASSWORD || '');
       assert.strictEqual(db.connectionProperties.database, process.env.POSTGRES_DATABASE);
-    });
-
-    it('throws error when type is invalid', function () {
-      assert.throws(function () { naomi.create('invalid'); }, /database type must be one of mysql, postgres/i);
-      assert.throws(function () { naomi.create(123); }, /database type must be a string/i);
-      assert.throws(function () { naomi.create(false); }, /database type must be a string/i);
-      assert.throws(function () { naomi.create(null); }, /database type must be a string/i);
-      assert.throws(function () { naomi.create({}); }, /database type must be a string/i);
     });
 
   });
