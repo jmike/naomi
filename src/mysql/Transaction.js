ba783b88d61761526404e2e59258b7d1ae50414b
Dimitrios C. Michalakos <dimitris@jmike.gr>
2015-02-27 18:15:36 +0200

    Add Transaction#rollback

--- src/mysql/Transaction.js
+++ src/mysql/Transaction.js
@@ -1,6 +1,4 @@
 var Promise = require('bluebird');
-var _ = require('lodash');
-var type = require('type-of');
 
 /**
  * Creates a new Transaction with the designated database.
@@ -58,6 +56,29 @@ Transaction.prototype.commit = function (callback) {
 };
 
 /**
+ * Rolls back the transaction.
+ * Please note: transaction will become effectively useless after calling this method.
+ * @param {Function} [callback] an optional callback function. with (err, transaction) arguments
+ * @return {Promise} resolving to this transaction
+ */
+Transaction.prototype.rollback = function (callback) {
+  var _this = this;
+
+  if (this.client === null) {
+    return Promise.reject(new Error('Invalid transaction state; transaction not yet started'));
+  }
+
+  return this.query('ROLLBACK;')
+    .then (function () {
+      _this.db.releaseClient(_this.client);
+      _this.client = null;
+    })
+    .return(_this)
+    .bind(this)
+    .nodeify(callback);
+};
+
+/**
  * Runs the given parameterized SQL statement as part of the transaction.
  * @param {String} sql parameterized SQL statement
  * @param {Array} [params] an array of parameter values
@@ -66,77 +87,11 @@ Transaction.prototype.commit = function (callback) {
  * @returns {Promise} resolving to the query results
  */
 Transaction.prototype.query = function (sql, params, options, callback) {
-  var _this = this;
-  var resolver;
-
-  // validate sql argument
-  if (!_.isString(sql)) {
-    throw new Error('Invalid sql argument; expected string, received ' + type(sql));
-  }
-
-  // handle optional params argument
-  if (_.isFunction(params)) {
-    callback = params;
-    options = undefined;
-    params = [];
-  } else if (_.isPlainObject(params)) {
-    callback = options;
-    options = params;
-    params = [];
-  } else if (_.isUndefined(params)) {
-    callback = undefined;
-    options = undefined;
-    params = [];
-  }
-
-  // validate params argument
-  if (!_.isArray(params)) {
-    throw new Error('Invalid params argument; expected array, received ' + type(params));
-  }
-
-  // handle optional options argument
-  if (_.isFunction(options)) {
-    callback = options;
-    options = {};
-  } else if (_.isUndefined(options)) {
-    callback = undefined;
-    options = {};
-  }
-
-  // validate options argument
-  if (!_.isPlainObject(options)) {
-    throw new Error('Invalid options argument; expected object, received ' + type(options));
-  }
-
-  // make sure transaction has started
   if (this.client === null) {
     return Promise.reject(new Error('Invalid transaction state; transaction not yet started'));
   }
 
-  // check if options is specified
-  if (!_.isEmpty(options)) {
-    sql = _.assign(options, {sql: sql}); // merge with sql
-  }
-
-  // define promise resolver
-  resolver = function (resolve, reject) {
-    _this.client.query(sql, params, function (err, records) {
-      if (err) return reject(err);
-
-      // check if sql is SELECT statement
-      if (_.isArray(records)) {
-        resolve(records);
-      } else {
-        // sql is DML statement
-        resolve({
-          insertId: records.insertId,
-          affectedRows: records.affectedRows
-        });
-      }
-    });
-  };
-
-  return new Promise(resolver)
+  return this.db.queryClient(this.client, sql, params, options)
     .bind(this)
     .nodeify(callback);
 };
