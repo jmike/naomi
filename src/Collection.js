96a051d2479e1770e596b5c4d6fd65b26102f25d
Dimitrios C. Michalakos <dimitris@jmike.gr>
2016-02-21 12:08:52 +0200

    Refactor Collection methods

--- src/Collection.js
+++ src/Collection.js
@@ -33,9 +33,9 @@ class Collection extends EventEmitter {
   }
 
   /**
-   * Reverse engineers the collection's schema from metadata retreived from the database.
+   * Reverse engineers the collection's schema from metadata retrieved from the database.
    * This function will update the collection's schema.
-   * @param {Function<Error>} [callback] an optional callback function
+   * @param {Function<Error>} [callback] an optional callback function.
    * @return {Promise}
    */
   reverseEngineer(callback: ?Function): Promise {
@@ -44,8 +44,8 @@ class Collection extends EventEmitter {
 
   /**
    * Registers the collection to the database, e.g. creates table in MySQL.
-   * @param {string} [name] optional collection name to register in database
-   * @param {Function<Error>} [callback] an optional callback function
+   * @param {string} [name] optional collection name to register in database.
+   * @param {Function<Error>} [callback] an optional callback function.
    * @return {Promise}
    */
   register(name: string | ?Function, callback: ?Function): Promise {
@@ -64,104 +64,20 @@ class Collection extends EventEmitter {
   }
 
   /**
-   * Retrieves an array of records from the collection based on the given query.
-   * @param {(boolean|number|string|Date|Object|Array<Object>)} [$query] a naomi query object
-   * @param {Function<Error, Array<Object>>} [callback] an optional callback function
-   * @returns {Promise<Array<Object>>} a bluebird promise resolving to an array of records
-   * @throws {TypeError} if arguments are of invalid type
+   * Retrieves an array of records from the collection based on the given selection.
+   * @param {(boolean|number|string|Date|Object|Array<Object>)} [selection] a naomi selection expression.
+   * @param {Object} [options] query options.
+   * @param {Function<Error, Array<Object>>} [callback] an optional callback function.
+   * @returns {Promise<Array<Object>>} a bluebird promise resolving to an array of records.
+   * @throws {TypeError} if arguments are of invalid type.
    */
-  find($query: boolean | number | string | ?Object, callback: ?Function): Promise {
-    // validate arguments
-    if (_.isNull($query)) {
-      throw new TypeError('Invalid $query argument; null is not allowed');
-    }
-
+  find(selection: boolean | number | string | Object | ?Function, options: Object | ?Function, callback: ?Function): Promise {
     // handle optional arguments
-    if (_.isFunction($query)) {
-      callback = $query;
-      $query = undefined;
+    if (_.isFunction(selection)) {
+      callback = selection;
+      selection = undefined;
+      options = {};
     }
-
-    return Promise.resolve().nodeify(callback);
-  }
-
-  /**
-   * Retrieves a single record from the collection based on the given query.
-   * @param {(boolean|number|string|Date|Object|Array<Object>)} [$query] a naomi query object
-   * @param {Function<Error, Object>} [callback] an optional callback function
-   * @returns {Promise<Object>} a bluebird promise resolving to a single record
-   * @throws {TypeError} if arguments are of invalid type
-   */
-  findOne($query: boolean | number | string | ?Object, callback: ?Function): Promise {
-    // validate arguments
-    if (_.isNull($query)) {
-      throw new TypeError('Invalid $query argument; null is not allowed');
-    }
-
-    // handle optional arguments
-    if (_.isFunction($query)) {
-      callback = $query;
-      $query = undefined;
-    }
-
-    return Promise.resolve().nodeify(callback);
-  }
-
-  /**
-   * Counts records in the collection based on the given query.
-   * @param {(boolean|number|string|Date|Object|Array<Object>)} [$query] a naomi query object
-   * @param {Function<Error, number>} [callback] an optional callback function
-   * @returns {Promise<number>} a bluebird promise resolving to the number of records
-   * @throws {TypeError} if arguments are of invalid type
-   */
-  count($query: boolean | number | string | ?Object, callback: ?Function): Promise {
-    // validate arguments
-    if (_.isNull($query)) {
-      throw new TypeError('Invalid $query argument; null is not allowed');
-    }
-
-    // handle optional arguments
-    if (_.isFunction($query)) {
-      callback = $query;
-      $query = undefined;
-    }
-
-    return Promise.resolve().nodeify(callback);
-  }
-
-  /**
-   * Removes records from the collection based on the given query.
-   * @param {(boolean|number|string|Date|Object|Array<Object>)} [$query] a naomi query object
-   * @param {Function<Error>} [callback] an optional callback function
-   * @returns {Promise>} a bluebird promise
-   * @throws {TypeError} if arguments are of invalid type
-   */
-  remove($query: boolean | number | string | ?Object, callback: ?Function): Promise {
-    // validate arguments
-    if (_.isNull($query)) {
-      throw new TypeError('Invalid $query argument; null is not allowed');
-    }
-
-    // handle optional arguments
-    if (_.isFunction($query)) {
-      callback = $query;
-      $query = undefined;
-    }
-
-    return Promise.resolve().nodeify(callback);
-  }
-
-  /**
-   * Inserts the specified record(s) to the collection.
-   * @param {(Object|Array<Object>)} records the record(s) to insert to the collection
-   * @param {Object} [options] optional query options
-   * @param {Function<err, Object>} [callback] an optional callback function with (err, keys) argument
-   * @returns {Promise} a bluebird promise resolving to the primary key of the created record(s).
-   * @throws {TypeError} if arguments are of invalid type
-   */
-  insert(records: Object | Array, options: ?Object, callback: ?Function): Promise {
-    // handle optional arguments
-    options = options || {};
 
     if (_.isFunction(options)) {
       callback = options;
@@ -172,17 +88,138 @@ class Collection extends EventEmitter {
   }
 
   /**
-   * Creates or updates (if already exist) the specified record(s) in table.
-   * @param {(Object|Array<Object>)} records the record(s) to insert to the collection
-   * @param {Object} [options] optional query options
-   * @param {Function<err, Object>} [callback] an optional callback function with (err, keys) argument
-   * @returns {Promise} a bluebird promise resolving to the primary key of the created/updated record(s)
-   * @throws {TypeError} if arguments are of invalid type
+   * Retrieves a single record from the collection based on the given selection.
+   * @param {(boolean|number|string|Date|Object|Array<Object>)} [selection] an optional naomi selection expression.
+   * @param {Object} [options] query options.
+   * @param {Function<Error, Object>} [callback] an optional callback function
+   * @returns {Promise<Object>} a bluebird promise resolving to a single record.
+   * @throws {TypeError} if arguments are of invalid type.
    */
-  update(records: Object | Array, options: ?Object, callback: ?Function): Promise {
+  findOne(selection: boolean | number | string | Object | ?Function, options: Object | ?Function, callback: ?Function): Promise {
     // handle optional arguments
-    options = options || {};
+    if (_.isFunction(selection)) {
+      callback = selection;
+      selection = undefined;
+      options = {};
+    }
 
+    if (_.isFunction(options)) {
+      callback = options;
+      options = {};
+    }
+
+    return Promise.resolve().nodeify(callback);
+  }
+
+  /**
+   * Counts records in the collection based on the given selection.
+   * @param {(boolean|number|string|Date|Object|Array<Object>)} [selection] a naomi selection expression.
+   * @param {Object} [options] query options.
+   * @param {Function<Error, number>} [callback] an optional callback function.
+   * @returns {Promise<number>} a bluebird promise resolving to the number of records.
+   * @throws {TypeError} if arguments are of invalid type.
+   */
+  count(selection: boolean | number | string | Object | ?Function, options: Object | ?Function,, callback: ?Function): Promise {
+    // handle optional arguments
+    if (_.isFunction(selection)) {
+      callback = selection;
+      selection = undefined;
+      options = {};
+    }
+
+    if (_.isFunction(options)) {
+      callback = options;
+      options = {};
+    }
+
+    return Promise.resolve().nodeify(callback);
+  }
+
+  /**
+   * Removes records from the collection based on the given selection.
+   * @param {(boolean|number|string|Date|Object|Array<Object>)} [selection] an optional naomi selection expression.
+   * @param {Function<Error>} [callback] an optional callback function.
+   * @returns {Promise>} a bluebird promise.
+   * @throws {TypeError} if arguments are of invalid type.
+   */
+  remove(selection: boolean | number | string | Object | ?Function, options: Object | ?Function, callback: ?Function): Promise {
+    // handle optional arguments
+   if (_.isFunction(selection)) {
+      callback = selection;
+      selection = undefined;
+      options = {};
+    }
+
+    if (_.isFunction(options)) {
+      callback = options;
+      options = {};
+    }
+
+    return Promise.resolve().nodeify(callback);
+  }
+
+  /**
+   * Creates the specified record(s) in the collection.
+   * @param {(Object|Array<Object>)} records the record(s) to insert to the collection.
+   * @param {Object} [options] query options.
+   * @param {Function<err, Object>} [callback] an optional callback function with (err, keys) argument.
+   * @returns {Promise} a bluebird promise resolving to the primary key of the created record(s).
+   * @throws {TypeError} if arguments are of invalid type.
+   */
+  insert(records: Object, options: Object | ?Function, callback: ?Function): Promise {
+    // validate arguments
+    if (!_.isPlainObject(records) && !_.isArray(records)) {
+      throw new TypeError(`Invalid records argument; exprected object or array, received ${type(records)}`);
+    }
+
+    // handle optional arguments
+    if (_.isFunction(options)) {
+      callback = options;
+      options = {};
+    }
+
+    return Promise.resolve().nodeify(callback);
+  }
+
+  /**
+   * Creates, or updates if they already exist, the specified record(s) in the collection.
+   * @param {(Object|Array<Object>)} records the record(s) to insert to the collection.
+   * @param {Object} [options] query options.
+   * @param {Function<err, Object>} [callback] an optional callback function with (err, keys) argument.
+   * @returns {Promise} a bluebird promise resolving to the primary key of the created record(s).
+   * @throws {TypeError} if arguments are of invalid type.
+   */
+  upsert(records: Object, options: Object | ?Function, callback: ?Function): Promise {
+    // validate arguments
+    if (!_.isPlainObject(records) && !_.isArray(records)) {
+      throw new TypeError(`Invalid records argument; exprected object or array, received ${type(records)}`);
+    }
+
+    // handle optional arguments
+    if (_.isFunction(options)) {
+      callback = options;
+      options = {};
+    }
+
+    return Promise.resolve().nodeify(callback);
+  }
+
+  /**
+   * Updates (if already exist) the specified record(s) in the collection.
+   * @param {(boolean|number|string|Date|Object|Array<Object>)} selection a naomi selection expression.
+   * @param {(Object|Array<Object>)} records the record(s) to insert to the collection.
+   * @param {Object} [options] query options.
+   * @param {Function<err, Object>} [callback] an optional callback function with (err, keys) argument.
+   * @returns {Promise} a bluebird promise resolving to the primary key of the created/updated record(s).
+   * @throws {TypeError} if arguments are of invalid type.
+   */
+  update(selection, records: Object | Array, options: ?Object, callback: ?Function): Promise {
+    // validate arguments
+    if (!_.isPlainObject(records) && !_.isArray(records)) {
+      throw new TypeError(`Invalid records argument; exprected object or array, received ${type(records)}`);
+    }
+
+    // handle optional arguments
     if (_.isFunction(options)) {
       callback = options;
       options = {};
