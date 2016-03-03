0a30416b92b070086485ca8f065626bc4cb948c5
Dimitrios C. Michalakos <dimitris@jmike.gr>
2016-03-04 00:06:19 +0200

    Allow null values when nullable = true

--- src/datatypes/String.js
+++ src/datatypes/String.js
@@ -50,7 +50,7 @@ class StringType extends AnyType {
     if (this.props.lowercase) joi = joi.lowercase();
     if (this.props.uppercase) joi = joi.uppercase();
     if (this.props.trim) joi = joi.trim();
-    if (this.props.nullable) joi = joi.optional();
+    if (this.props.nullable) joi = joi.optional().allow(null);
     if (this.props.default) joi = joi.default(this.props.default);
 
     return joi;
