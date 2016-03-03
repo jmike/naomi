a89b37b183ca60334d75de19f6a00cf96c0f73d1
Dimitrios C. Michalakos <dimitris@jmike.gr>
2016-03-03 19:56:34 +0200

    Add unit test for integer datatype

---
+++ test/datatypes/integer.js
@@ -0,0 +1,76 @@
+/* global describe, it */
+
+import {assert} from 'chai';
+import Joi from 'joi';
+import IntegerType from '../../src/datatypes/Integer';
+
+describe('Integer datatype', function() {
+  it('accepts only integer values', function() {
+    const dt = new IntegerType();
+    const schema = dt.toJoi();
+
+    assert.doesNotThrow(() => Joi.assert(123, schema));
+    assert.throws(() => Joi.assert(1.1, schema));
+    assert.throws(() => Joi.assert('abc', schema));
+    assert.throws(() => Joi.assert(null, schema));
+    assert.throws(() => Joi.assert(true, schema));
+    assert.throws(() => Joi.assert({}, schema));
+    assert.throws(() => Joi.assert(new Date(), schema));
+  });
+
+  it('respects min property', function() {
+    const dt = new IntegerType();
+    dt.min = 100;
+    const schema = dt.toJoi();
+
+    assert.doesNotThrow(() => Joi.assert(100, schema));
+    assert.throws(() => Joi.assert(99, schema));
+  });
+
+
+  it('respects max property', function() {
+    const dt = new IntegerType();
+    dt.max = 100;
+    const schema = dt.toJoi();
+
+    assert.doesNotThrow(() => Joi.assert(100, schema));
+    assert.throws(() => Joi.assert(101, schema));
+  });
+
+  it('respects negative property', function() {
+    const dt = new IntegerType();
+    dt.negative = true;
+    const schema = dt.toJoi();
+
+    assert.doesNotThrow(() => Joi.assert(-1, schema));
+    assert.throws(() => Joi.assert(123, schema));
+    assert.throws(() => Joi.assert(0, schema));
+  });
+
+  it('respects positive property', function() {
+    const dt = new IntegerType();
+    dt.positive = true;
+    const schema = dt.toJoi();
+
+    assert.doesNotThrow(() => Joi.assert(1, schema));
+    assert.throws(() => Joi.assert(0, schema));
+    assert.throws(() => Joi.assert(-1, schema));
+  });
+
+  it('respects nullable property', function() {
+    const dt = new IntegerType();
+    dt.nullable = true;
+    const schema = dt.toJoi();
+
+    assert.doesNotThrow(() => Joi.assert(null, schema));
+    assert.doesNotThrow(() => Joi.assert(undefined, schema));
+  });
+
+  it('respects default property', function() {
+    const dt = new IntegerType();
+    dt.default = 10;
+    const schema = dt.toJoi();
+
+    assert.strictEqual(Joi.attempt(undefined, schema), 10);
+  });
+});
