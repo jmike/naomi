5672f9c23e213d2b78d122041fdab5ac9b5c4379
Dimitrios C. Michalakos <dimitris@jmike.gr>
2016-03-04 10:06:28 +0200

    Add handy methods from Naomi v1 to Schema

--- src/Schema.js
+++ src/Schema.js
@@ -71,20 +71,109 @@ class Schema {
     _.set(this.columns, key, dt);
   }
 
+  /**
+   * Indicates whether the specified column exists in schema.
+   * @param {string} key the name of the column.
+   * @returns {boolean}
+   */
+  hasColumn(key) {
+    return _.has(this.columns, key);
+  }
+
+  /**
+   * Indicates whether the specified column is automatically incremented.
+   * @param {string} key the name of the column.
+   * @returns {boolean}
+   */
+  isAutoInc(key) {
+    const column = _.get(this.columns, key);
+
+    // make sure column exists
+    if (_.isUndefined(column)) {
+      return false;
+    }
+
+    return column.props.autoinc === true;
+  }
+
+  /**
+   * Returns an array of column names specified in schema.
+   * @return {Array<string>}
+   */
   getColumnNames() {
-    return _.keys(this.definition);
+    return _.keys(this.columns);
   }
 
-  toJSON() {
-    return _.mapValues(this.definition, (datatype) => datatype.toJSON());
-  }
+  /**
+   * Indicates whether the specified column(s) represent a primary key.
+   * Primary keys may be compound, i.e. composed of multiple columns, hence the acceptance of multiple params in this function.
+   * This method will always return false until database is ready.
+   * @param {...string} columns the name of the columns.
+   * @returns {boolean}
+   * @example
+   */
+  // isPrimaryKey() {
+  //   var columns = Array.prototype.slice.call(arguments, 0);
+  //   return _.xor(this.primaryKey, columns).length === 0;
+  // }
 
-  toJoi() {
-    return Joi.object().strict(true).keys(_.mapValues(this.definition, (datatype) => datatype.toJoi()));
-  }
+  /**
+   * Indicates whether the specified column(s) represent a unique key.
+   * Unique keys may be compound, i.e. composed of multiple columns, hence the acceptance of multiple params.
+   * This method will always return false until database is ready.
+   * @param {...string} columns the name of the columns.
+   * @returns {boolean}
+   * @example
+   *
+   * table.isUniqueKey('pid');
+   */
+  // isUniqueKey() {
+  //   var columns = Array.prototype.slice.call(arguments, 0);
+  //   return _.some(this.uniqueKeys, function (e) {
+  //     return _.xor(e, columns).length === 0;
+  //   });
+  // }
 
-  validate(record: Object, callback: ?Function) {
-    if (!this.joi) this.joi = this.toJoi();
+  /**
+   * Indicates whether the specified column(s) represent an index key.
+   * Index keys may be compound, i.e. composed of multiple columns, hence the acceptance of multiple params.
+   * This method will always return false until database is ready.
+   * @param {...string} columns the name of the columns.
+   * @returns {boolean}
+   * @example
+   *
+   * table.isIndexKey('firstName', 'lastName');
+   */
+  // isIndexKey() {
+  //   var columns = Array.prototype.slice.call(arguments, 0);
+  //   return _.some(this.indexKeys, function (e) {
+  //     return _.xor(e, columns).length === 0;
+  //   });
+  // }
+
+  /**
+   * Indicates whether the table has a simple automatically incremented primary key.
+   * This method will always return false until database is ready.
+   * @returns {boolean}
+   * @example
+   *
+   * table.hasAutoIncPrimaryKey();
+   */
+  // hasAutoIncPrimaryKey() {
+  //   return this.primaryKey.length === 1 && this.isAutoInc(this.primaryKey[0]);
+  // }
+
+  /**
+   * Validates the designated record against this schema.
+   * @param {Object} record the record to validate.
+   * @param {Function<Error, Object>} callback an optional callback function.
+   * @return {Promise<Object>}
+   */
+  validate(record: Object, callback: ?Function): Promise {
+    // make sure joi exists
+    if (!this.joi) {
+      this.joi = this.toJoi(); // cache to instance property
+    }
 
     return new Promise((resolve, reject) => {
       Joi.validate(record, this.joi, {convert: false}, (err, value) => {
@@ -95,11 +184,21 @@ class Schema {
   }
 
   toMetaData(): Object {
-    return this.definition;
+    return this.columns;
   }
 
   static fromMetaData(obj: Object): Schema {
     return new Schema(obj);
+  }
+
+  toJoi() {
+    return Joi.object()
+      .strict(true)
+      .keys(_.mapValues(this.columns, (datatype) => datatype.toJoi()));
+  }
+
+  toJSON() {
+    return _.mapValues(this.columns, (datatype) => datatype.toJSON());
   }
 
 }
