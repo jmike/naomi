ef57ed671715df15213e56e4a893db85adf51b29
Dimitrios C. Michalakos <dimitris@jmike.gr>
2016-04-30 16:15:10 +0300

    Update test inder to run using the mocha cli

--- test/index.js
+++ test/index.js
@@ -1,39 +1,23 @@
-require('dotenv').load({silent: true});
+require('dotenv').load({ silent: true });
 require('babel-core/register');
 
-const path = require('path');
-const Mocha = require('mocha');
-
-// init mocha
-const mocha = new Mocha({
-  reporter: 'spec',
-  timeout: 30000 // 30 secs
-});
-
 // load the test files
-mocha.addFile(path.resolve(__dirname, './schema'));
-mocha.addFile(path.resolve(__dirname, './naomi'));
-mocha.addFile(path.resolve(__dirname, './database'));
-mocha.addFile(path.resolve(__dirname, './collection'));
-mocha.addFile(path.resolve(__dirname, './queryparsers/selection'));
-mocha.addFile(path.resolve(__dirname, './queryparsers/projection'));
-mocha.addFile(path.resolve(__dirname, './queryparsers/orderby'));
-mocha.addFile(path.resolve(__dirname, './queryparsers/limit'));
-mocha.addFile(path.resolve(__dirname, './queryparsers/offset'));
-mocha.addFile(path.resolve(__dirname, './datatypes/integer'));
-mocha.addFile(path.resolve(__dirname, './datatypes/float'));
-mocha.addFile(path.resolve(__dirname, './datatypes/number'));
-mocha.addFile(path.resolve(__dirname, './datatypes/string'));
-mocha.addFile(path.resolve(__dirname, './datatypes/uuid'));
-mocha.addFile(path.resolve(__dirname, './datatypes/enum'));
-mocha.addFile(path.resolve(__dirname, './datatypes/date'));
-mocha.addFile(path.resolve(__dirname, './datatypes/boolean'));
-mocha.addFile(path.resolve(__dirname, './datatypes/binary'));
-mocha.addFile(path.resolve(__dirname, './datatypes/email'));
-
-// run the tests
-mocha.run(function (failures) {
-  process.on('exit', function () {
-    process.exit(failures);
-  });
-});
+require('./schema');
+require('./naomi');
+require('./database');
+require('./collection');
+require('./queryparsers/selection');
+require('./queryparsers/projection');
+require('./queryparsers/orderby');
+require('./queryparsers/limit');
+require('./queryparsers/offset');
+require('./datatypes/integer');
+require('./datatypes/float');
+require('./datatypes/number');
+require('./datatypes/string');
+require('./datatypes/uuid');
+require('./datatypes/enum');
+require('./datatypes/date');
+require('./datatypes/boolean');
+require('./datatypes/binary');
+require('./datatypes/email');
